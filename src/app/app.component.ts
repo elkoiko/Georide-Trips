47e0fd5a2d0c78e1ee346a3944d045007a4ee10f
Thomas Couacault <tcouacault@gmail.com>
2023-07-16 19:21:13 +0200

    Add fake data to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GeorideAPIService } from "./services/georide-api.service";
+import { GeorideAPITrip } from "./models/GeorideAPI/GeorideAPI.type";
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,62 @@ import { GeorideAPIService } from "./services/georide-api.service";
 })
 export class AppComponent {
   title = 'Georide-Trips';
+  trips: GeorideAPITrip[] = [];
 
-  constructor() { }
+  constructor() {
+    this.trips.push({
+      id: 1,
+      trackerId: 1,
+      averageSpeed: 67,
+      distance: 12,
+      duration: 3,
+      startAddress: "Cesson-Sévigné",
+      niceStartAddress: "Cesson-Sévigné",
+      startLat: 1,
+      startLon: 1,
+      endAddress: "",
+      niceEndAddress: "Rennes",
+      endLat: 1,
+      endLon: 1,
+      startTime: new Date(),
+      endTime: new Date(new Date().getTime() + 1000 * 60 * 21),
+      staticImage: "https://georide.fr/wp-content/uploads/2022/11/3-%E2%80%93-2@2x@2x.png",
+    });
+    this.trips.push({
+      id: 1,
+      trackerId: 1,
+      averageSpeed: 67,
+      distance: 12,
+      duration: 3,
+      startAddress: "Cesson-Sévigné",
+      niceStartAddress: "Cesson-Sévigné",
+      startLat: 1,
+      startLon: 1,
+      endAddress: "",
+      niceEndAddress: "Rennes",
+      endLat: 1,
+      endLon: 1,
+      startTime: new Date(),
+      endTime: new Date(new Date().getTime() + 1000 * 60 * 21),
+      staticImage: "https://georide.fr/wp-content/uploads/2022/11/3-%E2%80%93-2@2x@2x.png",
+    });
+    this.trips.push({
+      id: 1,
+      trackerId: 1,
+      averageSpeed: 67,
+      distance: 12,
+      duration: 3,
+      startAddress: "Cesson-Sévigné",
+      niceStartAddress: "Cesson-Sévigné",
+      startLat: 1,
+      startLon: 1,
+      endAddress: "",
+      niceEndAddress: "Rennes",
+      endLat: 1,
+      endLon: 1,
+      startTime: new Date(),
+      endTime: new Date(new Date().getTime() + 1000 * 60 * 21),
+      staticImage: "https://georide.fr/wp-content/uploads/2022/11/3-%E2%80%93-2@2x@2x.png",
+    });
+  }
 }
